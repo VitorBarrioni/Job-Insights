9354e04fde9f77ebd35e0aa8b69b89d7e6ced27a
VitorBarrioni <vitor_teixeira90@hotmail.com>
2023-05-07 15:11:20 -0300

    min salaries

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -14,21 +14,14 @@ def get_max_salary(path: str) -> int:
 
 
 def get_min_salary(path: str) -> int:
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    jobs_list = read(path)
+    return min(
+        [
+            int(jobs["min_salary"])
+            for jobs in jobs_list
+            if jobs["min_salary"].isnumeric()
+        ]
+    )
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
