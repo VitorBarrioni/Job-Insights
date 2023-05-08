f81737e3269bf37c87c2131e2e98b9d395f8f5de
VitorBarrioni <vitor_teixeira90@hotmail.com>
2023-05-08 10:31:12 -0300

    req10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -3,5 +3,5 @@ from src.pre_built.counter import count_ocurrences
 
 def test_counter():
     count_salary = 598
-    count_salary_ocurrences = count_ocurrences("data/jobs.csv", "jobs")
+    count_salary_ocurrences = count_ocurrences("data/jobs.csv", "salary")
     assert count_salary_ocurrences == count_salary
